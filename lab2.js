0accc828b6e642a8e6ad8225047f4e4e8a6500c4
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-03 16:25:53 +0800

    done lab2 n 3

--- lab2.js
+++ lab2.js
@@ -4,6 +4,13 @@ const calendar = {
   getDate: function () {
     return new Date().toDateString();
   },
+  getTime: function (){
+    return new Date().toLocaleTimeString();
+  }
 };
 
+
+
+
 console.log(calendar.getDate());
+console.log(calendar.getTime());
--- lab3.js
+++ lab3.js
@@ -2,9 +2,13 @@ const assertEquals = require("./assert-helper");
 // 1. use arrow functions to create 4 methods:
 // add, subtract, multiply, divide
 
-const add = (a, b) => {
-  return a + b;
-};
+const add = (a, b) => a + b;
+
+const subtract = (a,b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
 
 assertEquals(add(1, 2), 3);
 assertEquals(subtract(1, 2), -1);
